abef47534c8596c943f0b152e23c2fb65710a2e2
xKRISTOFx <74738911+xKRISTOFx@users.noreply.github.com>
2022-04-17 21:35:32 +0200

    +

--- static/zusel.js
+++ static/zusel.js
@@ -1,22 +1,23 @@
 var wtokuapi = false;
+
 $(document).ready(function () {
     $("#livebox").on("input", function (e) {
-        wtokuapi = true;
         if (!wtokuapi) {
-        $("#datalist").empty();
-        $.ajax({
-            method: "post",
-            url: "/search",
-            data: { nazwa_uzytkownika: $("#livebox").val() },
-            success: function (res) {
-                // var data = "<ul>";
-                // data += "<li>" + res + "</li>";
-                // data += "</ul>";
-                // $("#datalist").html(data);
-                console.log(res);
-            },
-        });
-        wtokuapi = false;
-    }
+            wtokuapi = true;
+            $("#datalist").empty();
+            $.ajax({
+                method: "post",
+                url: "/search",
+                data: { nazwa_uzytkownika: $("#livebox").val() },
+                success: function (res) {
+                    // var data = "<ul>";
+                    // data += "<li>" + res + "</li>";
+                    // data += "</ul>";
+                    // $("#datalist").html(data);
+                    console.log(res);
+                },
+            });
+            wtokuapi = false;
+        }
     });
 });
