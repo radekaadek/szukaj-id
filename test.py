1a1e16e57ce2df6c46cb8a89764b0249f254988b
radekaadek <radekaadek@gmail.com>
2022-04-17 20:23:35 +0200

    if name main w tescie

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 from steamwebapi.api import ISteamUser, IPlayerService, ISteamUserStats
 from steamwebapi import profiles
 
-steam_api_key = 'EE03692ACB03E4371522180E26926643'
 
-plik = open('wynik.txt', 'w')
-steamuserinfo = ISteamUser(steam_api_key=steam_api_key)
-steamid = steamuserinfo.resolve_vanity_url("radekaadek", format="json")['response']['steamid']
-usersummary = steamuserinfo.get_player_summaries(steamid, format="json")['response']['players'][0]
+def main():
+    steam_api_key = 'EE03692ACB03E4371522180E26926643'
+
+    plik = open('wynik.txt', 'w')
+    steamuserinfo = ISteamUser(steam_api_key=steam_api_key)
+    steamid = steamuserinfo.resolve_vanity_url("radekaadek", format="json")['response']['steamid']
+    usersummary = steamuserinfo.get_player_summaries(steamid, format="json")['response']['players'][0]
 
 
-plik.write(str(usersummary))
+    plik.write(str(usersummary))
 
-plik.close()
+    plik.close()
+
+if __name__ == '__main__':
+    main()
