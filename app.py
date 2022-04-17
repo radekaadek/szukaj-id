ab9a1809cf5b75166af8a9205fa47f03dd26551d
xKRISTOFx <74738911+xKRISTOFx@users.noreply.github.com>
2022-04-17 20:51:53 +0200

    usefull (#3)
    * ww
    
    * usefull
    
    Co-authored-by: radekaadek <77837063+radekaadek@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from steamwebapi.api import ISteamUser, IPlayerService, ISteamUserStats
+from steamwebapi import profiles
 
 app = Flask(__name__)
 
@@ -10,8 +12,13 @@ def index():
 @app.route("/search", methods = ["POST", "GET"])
 def search():
     nazwa_uzytkownika = request.form["nazwa_uzytkownika"]
-    print(nazwa_uzytkownika)
-    return nazwa_uzytkownika
+
+    steam_api_key = 'EE03692ACB03E4371522180E26926643'
+    steamuserinfo = ISteamUser(steam_api_key=steam_api_key)
+    steamid = steamuserinfo.resolve_vanity_url(str(nazwa_uzytkownika), format="json")['response']['steamid']
+    usersummary = steamuserinfo.get_player_summaries(steamid, format="json")['response']['players'][0]
+
+    return usersummary
 
 if __name__ == "__main__":
     app.run(host="25.85.5.88", debug=True)
