8baaeffcda7fd14eb46946ba87d3892b6b1e3a2a
radekaadek <radekaadek@gmail.com>
2022-04-17 23:27:36 +0200

    commit

--- app.py
+++ app.py
@@ -5,20 +5,28 @@ import json
 
 app = Flask(__name__)
 
+steam_api_key = 'EE03692ACB03E4371522180E26926643'
+riot_api_key = 'RGAPI-872636ab-9da1-4355-8afa-a1ab624dd6f4'
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/search", methods = ["POST", "GET"])
 def search():
+    #dokumentacja: https://pypi.org/project/steamwebapi/
     nazwa_uzytkownika = request.form["nazwa_uzytkownika"]
 
-    steam_api_key = 'EE03692ACB03E4371522180E26926643'
-    steamuserinfo = ISteamUser(steam_api_key=steam_api_key)
+    steamuserinfo = ISteamUser(steam_api_key = steam_api_key)
+    steamplayerinfo = IPlayerService(steam_api_key = steam_api_key, format="json")
     steamid = steamuserinfo.resolve_vanity_url(str(nazwa_uzytkownika), format="json")['response']['steamid']
-    usersummary = steamuserinfo.get_player_summaries(steamid, format="json")['response']['players'][0]
+    usersummary = steamuserinfo.get_player_summaries(steamid, format="json")
+    steamgamesinfo = steamplayerinfo.get_owned_games(steamid)
+    
     zwrot = {"steam":usersummary}
-    return zwrot
+    return steamgamesinfo
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
