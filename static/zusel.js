1d51820d6448fbe31d23b70b55bf9ebb3c1f661b
xKRISTOFx <gral1322@gmail.com>
2022-04-19 01:42:05 +0200

    dodanie kilku bajerów ale jeszcze nie działa

--- static/zusel.js
+++ static/zusel.js
@@ -1,19 +1,36 @@
 var wtokuapi = false;
+let favgamesplaytime = [];
 
+function newline(input) {
+    const newLI = document.createElement("li");
+    const profileAvatar = document.createElement("a");
+    const profileLink = document.createElement("a")
+    
+    profileLink.innerText = input.personaname
+    profileLink.href = input.url
+    profileLink.classList.add('profile-link');
+
+    profileAvatar.style.backgroundImage = `url(${input.avatar})`;
+    profileAvatar.style.width = "184px";
+    profileAvatar.style.height = "184px";
+    profileAvatar.href = input.avatar;
+
+
+    
+    newLI.appendChild(profileAvatar);
+    newLI.appendChild(profileLink);
+}
 
 function crHtml(res) {
     const datalistContainerDiv = document.querySelector("body #datalists_contaier");
+    datalistContainerDiv.innerHTML = "";
+    const newUL = document.createElement("UL");
     for (const d in res) {
-        const newUL = document.createElement("UL");
-        for (const r in res[d]) {
-            const newLI = document.createElement("li");
-            newLI.innerText = JSON.stringify(res[d][r]);
-            newUL.appendChild(newLI);
-        }
-        newUL.id = d + "List"
-        datalistContainerDiv.appendChild(newUL);
+        const newLine = newline(d);
+        newLine.id = d + "Line"
+        newUL.appendChild(newLine);
     }
-
+    datalistContainerDiv.appendChild(newUL);
 }
 
 $(document).ready(async function () {
@@ -31,7 +48,10 @@ $(document).ready(async function () {
                         $("#attentionMessage").css({ display: "none" })    
                         crHtml(res);
                         console.log(res);}
-                        else $("#attentionMessage").css({ display: "block" })
+                        else {
+                            $("#attentionMessage").css({ display: "block" })
+                            $("#datalists_contaier").empty();
+                        }
                     },
                 });
                 wtokuapi = false;
