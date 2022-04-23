b02aab0dd48e9d3ca4e8deb722e98813dc4a87e0
radekaadek <radekaadek@gmail.com>
2022-04-23 23:04:12 +0200

    aa

--- pitagoras.py
+++ pitagoras.py
@@ -2,13 +2,14 @@ def prime(n):
     i=2
     if n%i==0:
         return False
+    i += 1
     while i*i <= n:
         if n % i == 0:
             return False
         i += 2
-    return True 
+    return True
 
     #print first 100 prime numbers
-for i in range(1, 100000):
+for i in range(1, 1000000):
     if prime(i):
         print(i)
