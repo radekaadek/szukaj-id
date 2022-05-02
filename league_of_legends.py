eee901fb82cd3fd27201a6a2f71ea4571802f4d7
radekaadek <radekaadek@gmail.com>
2022-05-02 22:11:42 +0200

    api lol

--- league_of_legends.py
+++ league_of_legends.py
@@ -2,7 +2,7 @@ from riotwatcher import LolWatcher, ApiError
 
 #dokumentacja: https://riot-watcher.readthedocs.io/en/latest/index.html
 
-riot_api_key = 'RGAPI-b482cc84-b109-45b6-9302-f5638aba2198'
+riot_api_key = 'RGAPI-734283e5-0654-41c0-a79a-e0dc6a7835bc'
 lol_watcher = LolWatcher(riot_api_key)
 
 regiony = {'Brasil': 'BR1', 'Europe Nordic & East': 'EUN1', 'Europe West': 'EUW1', 'Japan': 'JP1', 'Korea': 'KR', 'Latin America North': 'LA1', 'Latin America South': 'LA2', 'North America': 'NA1', 'Oceania': 'OC1', 'Russia': 'RU', 'Turkey': 'TR1'}
