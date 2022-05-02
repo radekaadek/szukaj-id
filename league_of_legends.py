a4497ae60c37182343ec8c51c964ebb1d494bf8a
radekaadek <radekaadek@gmail.com>
2022-05-02 21:20:34 +0200

    naprawa kaczowania errora w range lol

--- league_of_legends.py
+++ league_of_legends.py
@@ -54,14 +54,17 @@ class player:
     def ranga(self):
         try:
             lista = lol_watcher.league.by_summoner(self.region, self.uzytkownik['id'])
-            if len(lista) != 5:
+            print(type(lista))
+            if not isinstance(lista, list):
                 return [None for _ in range(5)]
             for element in lista:
                 if element['queueType'] == 'RANKED_SOLO_5x5':
                     return [element['tier'], element['rank'], element['leaguePoints'], element['wins'], element['losses']]
-        except:
+        except Exception as error:
+            print(error)
             return [None for _ in range(5)]
-    
+
+
 # For Riot's API, the 404 status code indicates that the requested data wasn't found and
 # should be expected to occur in normal operation, as in the case of a an
 # invalid summoner name, match ID, etc.
