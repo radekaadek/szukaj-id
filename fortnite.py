d678f401709a2ab603d5061f8d8801b16dc54dfa
radekaadek <radekaadek@gmail.com>
2022-05-02 20:41:35 +0200

    eserror

--- fortnite.py
+++ fortnite.py
@@ -2,23 +2,27 @@ import fortnite_api
 
 api = fortnite_api.FortniteAPI('b4dab92b-ac98-4d0f-8cc9-5e2bc93de384')
 
-
+#dokumentacja: https://dash.fortnite-api.com/endpoints/stats
 #['image_url', 'raw_data', 'stats', 'user']
 #[26:]
 #.raw_data['all']['overall']['kd']
 #'ninja' - niepubliczny
 
-# print(dir(api.stats.fetch_by_name('elyzy').stats)[26:])
-# print(api.stats.fetch_by_name('elyzy').raw_data)
+
+print(api.stats.fetch_by_name('elyzy').raw_data)
 
 
 class Gracz_fortnite:
     def __init__(self, username, platform='epic'):
         self.username = username
         self.platform = platform
-        self.player_base = api.stats.fetch_by_name(username).raw_data
-        self.player = self.player_base['stats']['all']['overall']
-    
+        try:
+            self.player_base = api.stats.fetch_by_name(username).raw_data
+            self.player = self.player_base['stats']['all']['overall']
+        except Exception as error:
+            self.player_base = error
+            self.player = error
+
     def player_name(self):
         return self.player_base['account']['name']
 
@@ -34,3 +38,6 @@ class Gracz_fortnite:
     def last_played(self):
         return self.player['lastModified']
     
+print(Gracz_fortnite('ninja'))
+
+
