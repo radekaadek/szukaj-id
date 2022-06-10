96bf62f5cd4631fc3a5c6783af286b330b5dfa86
radekaadek <radekaadek@gmail.com>
2022-06-10 23:00:22 +0200

    hypixel beta

--- hypixel.py
+++ hypixel.py
@@ -1,8 +1,11 @@
 import requests
+from ast import literal_eval
+from base64 import b64decode
 
 hypixel_url = 'https://api.hypixel.net'
 hypixel_api_key = '36da01f0-28b9-4bc1-9c33-d24c57f55399'
 mojang_url = 'https://api.mojang.com/users/profiles/minecraft/'
+mojang_skin_url = 'https://sessionserver.mojang.com/session/minecraft/profile' #uuid
 
 username = 'radekaadek'
 
@@ -13,18 +16,33 @@ def faster(route, uuid) -> dict:
     faster_data = faster_json.json()
     return faster_data
 
-
-
+# rank, aliases last_seen values can be 'ZAMKOR' if not found,
+# this depends on the age of a players account
 def dane(username) -> dict:
     uuid = requests.get(f'{mojang_url}{username}').json()['id']
+    #hypixel data
     player_status = faster('status', uuid)['session']['online']
     stats = faster('player', uuid)
-    print(stats['player'])
-    for key, value in stats['player'].items() :
-        print(f'{key} {value}')
-    aliases = stats['player']['knownAliases']
-    last_seen = stats['player']['lastLogout']
-    return {'status': player_status, 'last_seen': last_seen, 'aliases': aliases}
+    try:
+        rank = stats['player']['rank']
+    except:
+        rank = 'ZAMKOR'
+    # print(stats['player'])
+    # for key, value in stats['player'].items() :
+    #     print(f'{key} {value}')
+    try:
+        aliases = stats['player']['knownAliases']
+    except:
+        aliases = 'ZAMKOR'
+    try:
+        last_seen = stats['player']['lastLogout']
+    except:
+        last_seen = 'ZAMKOR'
+    #skin
+    mojang_data = requests.get(f'{mojang_skin_url}/{uuid}').json()
+    decoded_string = b64decode(mojang_data['properties'][0]['value']).decode('utf-8')
+    skin_url = literal_eval(decoded_string)['textures']['SKIN']['url']
+    return {'status': player_status, 'last_seen': last_seen, 'aliases': aliases, 'rank': rank, 'skin_url': skin_url}
 
 
 print(dane(username))
