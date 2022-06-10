d61d127c645b3378eb048d6260d170c7789526ba
radekaadek <radekaadek@gmail.com>
2022-06-10 23:27:37 +0200

    hypixel beta with skins :O

--- hypixel.py
+++ hypixel.py
@@ -9,6 +9,25 @@ mojang_skin_url = 'https://sessionserver.mojang.com/session/minecraft/profile' #
 
 username = 'radekaadek'
 
+
+# returns a players default skin name if they have one
+# credit: https://github.com/crafatar/crafatar/blob/9d2fe0c45424de3ebc8e0b10f9446e7d5c3738b2/lib/skins.js#L90-L108
+def default_skin(uuid) -> str:
+    if (len(uuid)<= 16):
+        return 'steve'
+    lsbs_even = (
+                    int(uuid[7], 16) ^
+                    int(uuid[15], 16) ^
+                    int(uuid[23], 16) ^
+                    int(uuid[31], 16)
+                )
+    if lsbs_even:
+        return 'alex'
+    else:
+        return 'steve'
+
+
+
 # returns a dict of data from the api
 def faster(route, uuid) -> dict:
     PARAMS = {'key': hypixel_api_key, 'uuid': uuid}
@@ -18,6 +37,7 @@ def faster(route, uuid) -> dict:
 
 # rank, aliases last_seen values can be 'ZAMKOR' if not found,
 # this depends on the age of a players account
+# if the skin is default, it will return 'default': True, in this case the skin has a diffrent size
 def dane(username) -> dict:
     uuid = requests.get(f'{mojang_url}{username}').json()['id']
     #hypixel data
@@ -41,8 +61,16 @@ def dane(username) -> dict:
     #skin
     mojang_data = requests.get(f'{mojang_skin_url}/{uuid}').json()
     decoded_string = b64decode(mojang_data['properties'][0]['value']).decode('utf-8')
-    skin_url = literal_eval(decoded_string)['textures']['SKIN']['url']
-    return {'status': player_status, 'last_seen': last_seen, 'aliases': aliases, 'rank': rank, 'skin_url': skin_url}
+    default_skin = False
+    try:
+        skin_url = literal_eval(decoded_string)['textures']['SKIN']['url']
+    except:
+        default_skin = True
+        if default_skin(uuid) == 'alex':
+            skin_url = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/4b/Alex_%28texture%29_JE1_BE1.png/revision/latest?cb=20201025200833'
+        else:
+            skin_url = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d1/Steve_%28texture%29_JE4_BE2.png/revision/latest?cb=20210509095344'
+    return {'status': player_status, 'last_seen': last_seen, 'aliases': aliases, 'rank': rank,'default_skin': default_skin, 'skin_url': skin_url}
 
 
 print(dane(username))
