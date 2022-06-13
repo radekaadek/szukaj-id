9ffbac3b82bd1c1a0ad9ea7607bd017c7bc66e04
radekaadek <radekaadek@gmail.com>
2022-06-13 20:00:14 +0200

    fortnite z asynkiem! :D

--- fortnite.py
+++ fortnite.py
@@ -1,4 +1,5 @@
-import requests
+import aiohttp
+import asyncio
 
 fortnite_api_key = 'b4dab92b-ac98-4d0f-8cc9-5e2bc93de384'
 fortnite_api_website = 'https://fortnite-api.com/v2/stats/br/v2'
@@ -11,19 +12,23 @@ fortnite_api_website = 'https://fortnite-api.com/v2/stats/br/v2'
 
 username = 'radekaadek'
 
-def dane(username, platform='epic') -> dict:
-    params = {'accountType': platform, 'name': username}
-    headers = {'Authorization': fortnite_api_key}
-    raw_response = requests.get(fortnite_api_website, params=params, headers=headers).json()
-    if raw_response['status'] == 403:
-        # players account stats are private
-        return {'error': 'PRIVATE'}
-    if raw_response['status'] == 404:
-        # player not found
-        return {'error': 'NOT_FOUND'}
-    name = raw_response['data']['account']['name']
-    print(raw_response['data']['account'])
-    return {'error': 'OK', 'name': name, 'raw_data': raw_response['data']['account']}
+async def dane(username, platform='epic') -> dict:
+    async with aiohttp.ClientSession() as session:
+        params = {'accountType': platform, 'name': username}
+        headers = {'Authorization': fortnite_api_key}
+        async with session.get(fortnite_api_website, params=params, headers=headers) as response:
+            json_response = await response.json()
+            if json_response['status'] == 403:
+                # players account stats are private
+                return {'error': 'PRIVATE'}
+            if json_response['status'] == 404:
+                # player not found
+                return {'error': 'NOT_FOUND', 'name': '', 'raw_data': ''}
+            name = json_response['data']['account']['name']
+    print({'error': 'OK', 'name': name, 'raw_data': json_response['data']['account']})
+    return {'error': 'OK', 'name': name, 'raw_data': json_response['data']['account']}
     
 
-dane(username)
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(dane(username))
