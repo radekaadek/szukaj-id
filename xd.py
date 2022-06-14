afaf1989435b9ba872b73c8971a83a5f15e79a49
xKRISTOFx mobile <gral1322@gmail.com>
2022-06-14 21:43:51 +0200

    -xd

--- xd.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import httpx
-
-async def main():
-    async with httpx.AsyncClient() as client:
-        response = await client.get('https://www.example.com/')
-        print(response)
-
-asyncio.run(main())
