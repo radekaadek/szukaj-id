e43c0b68882e549dbe6df89fc592779a3286da74
xKRISTOFx mobile <gral1322@gmail.com>
2022-06-17 20:29:36 +0200

    +

--- steam.py
+++ steam.py
@@ -50,12 +50,12 @@ async def checkSteam(username, session):
         steamgamesinfo = arrayToDictionary(steamgamesinfo)
         
         for a in steamgamesinfo:
-            del a["playtime_windows_forever"]
-            del a["playtime_mac_forever"]
-            del a["playtime_linux_forever"]
-            a['icon_link'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + str(a['appid']) + '/' + str(a['img_icon_url'])
-            del a["img_icon_url"]
-            del a["appid"]
+            del steamgamesinfo[a]["playtime_windows_forever"]
+            del steamgamesinfo[a]["playtime_mac_forever"]
+            del steamgamesinfo[a]["playtime_linux_forever"]
+            steamgamesinfo[a]['icon_link'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/' + str(a['appid']) + '/' + str(a['img_icon_url'])
+            del steamgamesinfo[a]["img_icon_url"]
+            del steamgamesinfo[a]["appid"]
 
         print(steamgamesinfo)
 
