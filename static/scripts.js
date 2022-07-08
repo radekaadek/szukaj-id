64c7b8dcb63ac35354180ce5499ff675c9196805
xKRISTOFx <gral1322@gmail.com>
2022-07-08 23:41:44 +0200

    poprawki JS

--- static/scripts.js
+++ static/scripts.js
@@ -1,23 +1,40 @@
-{let loadingScreen = function () {
-  $(window).on("load", function () {
-      $(".loader-wrapper").fadeOut("slow");
-  });
-}
+{
+  //loading screen
+  let loadingScreen = function () {
+      $(window).on("load", function () {
+          $(".loader-wrapper").fadeOut("slow");
+      });
+  };
 
-let redirFu = function () {
+  // Search Bar
+  let redirFu = function () {
       window.location.replace("../" + $("#webSearchInput").val());
       $(".loader-wrapper").fadeIn("slow");
-}
+  };
 
-// Search Bar
-$(document).ready(() => {
-  document.querySelector("#webSearchInput").addEventListener("keypress",(e)=>{
-    if (e.key === "Enter") {
-        redirFu();
-    }
-});
+  let searchBar = () => {
+      document.querySelector("#webSearchInput").addEventListener("keypress", (e) => {
+          if (e.key === "Enter") {
+              redirFu();
+          }
+      });
+      document.querySelector(".web-search-submit").addEventListener("click", redirFu);
+  };
+  let paypalBar = async function () {
+      PayPal.Donation.Button({
+          env: "production",
+          hosted_button_id: "948Y2AG949RVY",
+          image: {
+              src: "https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif",
+              alt: "Donate with PayPal button",
+              title: "PayPal - The safer, easier way to pay online!",
+          },
+      }).render("#donateButton");
+  };
 
-  document.querySelector(".web-search-submit").addEventListener("click", redirFu);
-  loadingScreen();
-});
+  $(document).ready(() => {
+      searchBar();
+      paypalBar();
+      loadingScreen();
+  });
 }
