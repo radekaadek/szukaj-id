db186b98ea49b2d84767cebc9f43aa8cb4e8a01d
xKRISTOFx <gral1322@gmail.com>
2022-07-11 00:21:37 +0200

    debug dont cry

--- snapchat.py
+++ snapchat.py
@@ -9,7 +9,9 @@ html = urlopen(site)
 bs = BeautifulSoup(html, 'html.parser')
 
 profile_card = bs.find('span', class_ = 'UserDetailsCard_title__trfvf UserDetailsCard_oneLineTruncation__uhOF5')
-print(profile_card.string.encode('unicode-escape').decode('utf-8'))
+elem=str(profile_card.string)
+print(elem)
+print(type(elem))
 
 bitmoji_link = f'https://app.snapchat.com/web/deeplink/snapcode?username={username}&type=SVG&bitmoji=enable'
 
