b57fad0266b04909b7ed83b6944224c38b628c94
radekaadek <radekaadek@gmail.com>
2022-07-11 12:34:42 +0200

    removed keys

--- api_keys.py
+++ api_keys.py
@@ -1,4 +1,4 @@
-steam_api_key = 'EE03692ACB03E4371522180E26926643'
-riot_api_key = 'RGAPI-65585be8-4796-4c85-a4bf-f4734bfbd6f9'
-fortnite_api_key = 'b4dab92b-ac98-4d0f-8cc9-5e2bc93de384'
-hypixel_api_key = '36da01f0-28b9-4bc1-9c33-d24c57f55399'
+steam_api_key = ''
+riot_api_key = ''
+fortnite_api_key = ''
+hypixel_api_key = ''
